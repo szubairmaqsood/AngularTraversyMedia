af3307650136b3659db170ab8598b05ade06a570
zubair-pc\zubair <szubair@gmail.com>
2021-06-25 16:29:55 +0500

    A header component is made and it is used in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Coomponents } from './coomponents.header/coomponents.header.component';
-import { HeaderComponent } from './coomponents/header/header.component';
+import { HeaderComponent } from './component/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Coomponents.HeaderComponent,
-    HeaderComponent
+    HeaderComponent,
+   
   ],
   imports: [
     BrowserModule,
