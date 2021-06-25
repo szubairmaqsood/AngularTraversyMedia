c9998e3d58756f3d8b1fac5600671d53f1a56774
zubair-pc\zubair <szubair@gmail.com>
2021-06-25 15:59:19 +0500

    A header compoent folder is made using command ng generate compnent component/header In type script file of app component a property name title is present This property named title is used in html file of app component using {{}} like expresssions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Coomponents } from './coomponents.header/coomponents.header.component';
+import { HeaderComponent } from './coomponents/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Coomponents.HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
