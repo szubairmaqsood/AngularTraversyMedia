4e5fb7948aca49fbd2fc0ded3f4c5ec9f93cc0c9
zubair-pc\zubair <szubair@gmail.com>
2021-06-28 17:48:15 +0500

    New Record is  added to server

--- src/app/component/tasks/tasks.component.ts
+++ src/app/component/tasks/tasks.component.ts
@@ -28,4 +28,8 @@ toggleReminder(task:Task){
   this.taskService.updateTaskReminder(task).subscribe();
 }
 
+addTask(task:Task){
+  this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+}
+
 }
