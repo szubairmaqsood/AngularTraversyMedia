4bdb97bdb790eb3667ce276d3ce31528b22a458a
zubair-pc\zubair <szubair@gmail.com>
2021-06-28 19:14:52 +0500

    use of ngif and ui service to open and close form

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'Task Tracker';
-  constructor() { }
+  showAddTask:boolean=true;
+  subscription:Subscription;
+
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value =>(this.showAddTask = value))
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-    console.log('toggle');
+    //console.log('toggle');
+    this.uiService.toggleAddTask();
+
   }
 
 }
