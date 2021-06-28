0094a08b1cd1b1c89051e591d37e899baeb8ca47
zubair-pc\zubair <szubair@gmail.com>
2021-06-28 15:21:10 +0500

    Delete functionality is added 1. To delete from UI and from server 2. Conditional border in case of is to remember

--- src/app/component/task-item/task-item.component.ts
+++ src/app/component/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -18,11 +18,16 @@ export class TaskItemComponent implements OnInit {
   reminder: true,
 }; 
 
+@Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
 faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task1:Task){
+      this.onDeleteTask.emit(task1);
   }
 
 }
